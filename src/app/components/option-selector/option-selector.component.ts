53e2b9dc15c48f162a2f2409648707873e7e021b
HizemMontassar <hizemontassar@gmail.com>
2026-02-24 14:51:39 +0100

    clean up option selector component

--- src/app/components/option-selector/option-selector.component.ts
+++ src/app/components/option-selector/option-selector.component.ts
@@ -1,44 +1,27 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BoxSelectionService } from '../../state/box-selection.service';
-import { Observable, Subject, withLatestFrom } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { Option } from '../../models/option.model';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { OptionItemComponent } from '../option-item/option-item.component';
 
 @Component({
   selector: 'app-option-selector',
   templateUrl: './option-selector.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, OptionItemComponent],
 })
 export class OptionSelectorComponent {
   private readonly boxSelectionService = inject(BoxSelectionService);
-  private readonly destroyRef = inject(DestroyRef);
 
   selectedBoxId$!: Observable<number | null>;
   options$!: Observable<Option[]>;
   selectionHistory$!: Observable<Record<number, Option | null>>;
-  private readonly optionSelectClick$ = new Subject<Option>();
 
   ngOnInit() {
     this.selectedBoxId$ = this.boxSelectionService.selectedBoxId$;
     this.options$ = this.boxSelectionService.options$;
     this.selectionHistory$ = this.boxSelectionService.selectionHistory$;
-
-    this.optionSelectClick$
-      .pipe(
-        withLatestFrom(this.boxSelectionService.selectedBoxId$),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe(([option, boxId]) => {
-        if (boxId !== null) {
-          this.boxSelectionService.selectOption(boxId, option);
-        }
-      });
-  }
-
-  onOptionSelect(option: Option) {    
-    this.optionSelectClick$.next(option);
   }
 }
